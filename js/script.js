d66065d4db347ce2910585b84c75474067fe2a70
meiradev <rafameira10@icloud.com>
2022-09-05 16:06:34 -0300

    feat: add AlertError

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
 
 const form = document.querySelector("form");
 const inputWeight = document.getElementById("weight");
@@ -10,6 +11,15 @@ form.onsubmit = event => {
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if(showAlertError){
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
@@ -17,7 +27,9 @@ form.onsubmit = event => {
   Modal.open()
 } 
 
-
+function notANumber(value) {
+  return isNaN(value) || value == ""
+}
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
