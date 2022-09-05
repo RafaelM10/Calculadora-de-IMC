67b5dd611651af207e98b682755e6a6fdc9e637d
meiradev <rafameira10@icloud.com>
2022-09-05 13:55:35 -0300

    add: module

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
+import { Modal } from './modal.js'
+
 const form = document.querySelector("form");
 const inputWeight = document.getElementById("weight");
 const inputHeight = document.getElementById("height");
-
-const Modal = {
-  wrapper:  document.querySelector(".modal-wrapper"),
-  message:  document.querySelector(".modal .title span"),
-  buttonClose:  document.querySelector(".modal button.close"),
-  open() {
-    Modal.wrapper.classList.add("open")
-  },
-  close() {
-    Modal.wrapper.classList.remove("open")
-  },
-}
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -25,12 +15,9 @@ form.onsubmit = event => {
 
   Modal.message.innerText = message
   Modal.open()
-  
 } 
 
-Modal.buttonClose.onclick = () => {
-  Modal.close()
-}
+
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
